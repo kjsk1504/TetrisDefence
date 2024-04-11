91dcc2c2cad44b53a32477d92b572831fc8ae8a3
kjsk1504 <154114585+kjsk1504@users.noreply.github.com>
2024-04-11 09:02:49 +0900

    2024-04-10
    - PooledObject 수정

--- Assets/02.Scripts/Game/PooledObject.cs
+++ Assets/02.Scripts/Game/PooledObject.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TetrisDefence.Data.Manager;
 using UnityEngine;
 
@@ -6,12 +7,71 @@ namespace TetrisDefence.Game
 {
     public class PooledObject : MonoBehaviour
     {
+        [field: SerializeField] List<Transform> _transforms;
+
+
         private void Awake()
         {
-            string[] itemNames = Enum.GetNames(typeof(Item));
-            foreach (string itemName in itemNames)
+            if (transform.childCount > 0)
             {
-                new GameObject(itemName).transform.parent = transform;
+                foreach (Transform child in transform)
+                {
+                    if (Enum.IsDefined(typeof(Item), child.name))
+                    {
+                        child.SetSiblingIndex((int)Enum.Parse(typeof(Item), child.name));
+                    }
+                    else
+                    {
+                        child.gameObject.SetActive(false);
+                        Destroy(child.gameObject);
+                    }
+                }
+            }
+
+            _transforms = new List<Transform>(transform.GetComponentsInChildren<Transform>());
+            _transforms.Remove(transform);
+
+            var childNames = _transforms.ConvertAll<string>(x => x.name);
+            var itemNames = new List<string>(Enum.GetNames(typeof(Item)));
+
+            if (childNames != itemNames)
+            {
+                if (childNames.Count > itemNames.Count)
+                {
+                    for (int ix = 0; ix < childNames.Count; ix++)
+                    {
+                        if (ix < itemNames.Count)
+                        {
+                            _transforms[ix].name = itemNames[ix];
+                        }
+                        else
+                        {
+                            _transforms[ix].gameObject.SetActive(false);
+                            Destroy(_transforms[ix].gameObject);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int ix = 0; ix < itemNames.Count; ix++)
+                    {
+                        if (ix < childNames.Count)
+                        {
+                            _transforms[ix].name = itemNames[ix];
+                        }
+                        else
+                        {
+                            var newItem = new GameObject(itemNames[ix]);
+                            newItem.SetActive(true);
+                            newItem.transform.SetParent(transform);
+                            newItem.transform.SetSiblingIndex(ix);
+                        }
+                    }
+                }
+
+                _transforms = new List<Transform>(transform.GetComponentsInChildren<Transform>());
+                _transforms.Remove(transform);
+                _transforms.RemoveAll(x => !x.gameObject.activeSelf);
             }
         }
     }
